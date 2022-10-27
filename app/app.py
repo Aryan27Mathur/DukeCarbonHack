0b499c8ddfbec5310ed055a4a418e1d7eda19584
Aryan Mathur <34112101+Aryan27Mathur@users.noreply.github.com>
2022-10-27 02:59:00 +0000

    added list of names for grids in azure, uses simple get request to get emissions rating of central us

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, url_for, request
-import requests
+import requests, json
 from requests.auth import HTTPBasicAuth
 
 #settings stored as replit secrets
@@ -16,20 +16,18 @@ token = requests.get(login_url, auth=HTTPBasicAuth(wt_username, wt_password)).js
 
 
 #requesting GSF API
-data_url = 'https://api2.watttime.org/v2/data'
-headers = {'Authorization': 'Bearer {}'.format(token)}
-params = {'ba': 'CAISO_NORTH', 
-          'starttime': '2019-02-20T16:00:00-0800', 
-          'endtime': '2019-02-20T16:15:00-0800'}
+data_url = 'https://carbon-aware-api.azurewebsites.net/emissions/bylocation?location=centralus'
+headers = {'accept': 'application/json'}
+params = {'location': 'centralus'}
 rsp = requests.get(data_url, headers=headers, params=params)
-print(rsp.text)
+output = rsp.json()[0]['rating']
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template("base.html", apicall = rsp.text)
+    return render_template("base.html", apicall = output)
 
 
 app.run(host='0.0.0.0', port=81)
